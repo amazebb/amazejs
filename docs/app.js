ac496244b29dfb736ecb6cbcd4940bce0d3f438c
bb <26394523+broeknbytes@users.noreply.github.com>
2026-06-17 12:05:52 -0700

    docs: update amazejs import to use latest version
    - Changed CDN import URL from v0.5.0 to @latest

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,4 @@
-import { initTable } from 'https://cdn.jsdelivr.net/gh/amazebb/amazejs@v0.5.0/src/index.js';
+import { initTable } from 'https://cdn.jsdelivr.net/gh/amazebb/amazejs@latest/dist/amazejs.js';
 
 initTable({
     data: ['data/flat.json'],
